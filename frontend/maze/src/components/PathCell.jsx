61e9a57f18438307e62905ba8a233034d0608334
Swaraj Patade <swarajpatade586@gmail.com>
2025-10-23 20:37:42 +0530

    frontend

---
+++ frontend/maze/src/components/PathCell.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PathCell() {
+  return (
+    <div className='h-10 w-10 bg-amber-200'></div>
+  )
+}
+
+export default PathCell
---
+++ frontend/maze/src/components/WallCell.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function WallCell() {
+  return (
+    <div className='h-10 w-10 bg-amber-950'></div>
+  )
+}
+
+export default WallCell
